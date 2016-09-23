7ad4fad28d3bd9345796f72f2bbb86bcb858be98
rickyj37 <rickyjohnson37@gmail.com>
2016-09-23 09:58:14 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+// save this file in a folder called java you have created in home
+// Hello.java
+
+  class home {
+       public static void main (String a[]){
+           System.out.print("Hello world!");
+
+       }
+  } 
---
+++ Text2.java
@@ -0,0 +1,26 @@
+class Text1 {
+
+    public static void main (String a[]){
+            int i,ci;  // integer i
+            char c;  // charater c
+
+    i= 7;
+    c= 'a';
+    String s = "this is a string";
+    System.out.println("-------------------------");
+    System.out.println("i = "+i);
+    System.out.println("c = "+c);
+    System.out.println("s = "+c);
+        System.out.printlm("------------------------");
+    //do some math on the varaibles
+    i = i % 7;
+    ci = (int)c % 7; // type cast character c
+
+    System.out.println("-------------------------");
+    System.out.println("i = " + i);
+    System.out.println("ci = "+ci); 
+    ci = ci % 7;
+    Systemprintln("Now ci = "+ci);
+
+    }
+}
